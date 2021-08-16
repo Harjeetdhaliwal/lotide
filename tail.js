fbce348f25fd2dc05303c0ad9bfee8c3707120a5
Harjeet Kaur <harjeet.kr1995@gmail.com>
2021-08-16 18:36:36 -0400

    create tail function which returns all the array elements except the first one

---
+++ tail.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(array) {
+  return array.slice(1, array.length);
+};
+
+
+//Test Case 1
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2);
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], "Labs");
+
+//Test Case 2
+const result2 = tail([]);
+assertEqual(result2.length, 0);
+assertEqual(result2[0], undefined);

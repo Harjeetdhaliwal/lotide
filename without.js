f24d5b04d57fc3cccb796b59f4a952c19e54f237
Harjeet Kaur <harjeet.kr1995@gmail.com>
2021-08-17 18:10:36 -0400

    Created without function. It will return the susset of a given array by removing unwanted elements

---
+++ without.js
@@ -0,0 +1,37 @@
+const eqArrays = function(input1, input2) {
+  if (input1.length !== input2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < input1.length; i++) {
+      if (input1[i] !== input2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+const assertArraysEqual = function(input1, input2) {
+  const result = eqArrays(input1, input2);
+  if (result) {
+    console.log(`✅✅✅ Assertion Passed: ${input1} === ${input2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${input1} !== ${input2}`);
+  }
+}
+
+const without = function(source, itemsToRemove) {
+  let result = [];
+  for (let i = 0; i < source.length; i++) {
+    if (!(itemsToRemove.includes(source[i]))) {
+      result.push(source[i]);
+    }
+  }
+  return result;
+}
+
+console.log(without([1, 2, 3], [3, 4]));
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
